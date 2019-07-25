013207db9e0439506dfb229fc9e3bf95fb830ffc
Athir Saleem <noc7c9@gmail.com>
2019-07-25 11:23:41 +1000

    Create default eslint and prettier setups for non-project javascript scripts

---
+++ npm/.eslintrc.js
@@ -0,0 +1,20 @@
+// Default syntax checking and linting for Javascript
+//
+// Will be overwritten by project specific configuration if found
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {},
+};
---
+++ npm/.prettierrc.js
@@ -0,0 +1,9 @@
+// Default code style for Javascript
+//
+// Will be overwritten by project specific configuration if found
+module.exports = {
+    printWidth: 80,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+};
